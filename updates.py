5d2d5764aa5e14344b0de1764fdb4a94b891d656
Eliran Haziza <eliranhaze@gmail.com>
2019-02-20 04:13:43 -0500

    added update refs data functions

--- updates.py
+++ updates.py
@@ -60,3 +60,85 @@ def update_wittgenstein(refs):
                 changed = True
     return changed
 
+def update_williamson(refs):
+    changed = False
+    for r in refs:
+        if r['author'] == 'williamson t':
+            title = r['title']
+            if 'knowledge' in title and 'limits' in title:
+                year = '2000'
+                title = 'knowledge and its limits'
+            elif 'philosophy of philosophy' in title:
+                year = '2007'
+                title = 'the philosophy of philosophy'
+            elif title.startswith('vagueness'):
+                year = '1994'
+                title = 'vagueness'
+            if title != r['title']:
+                print 'updating %s %s' % (r['title'], r['year'])
+                r['year'] = year
+                r['title'] = title
+                changed = True
+    return changed
+
+def update_lewis(refs):
+    changed = False
+    for r in refs:
+        if r['author'] == 'lewis dk':
+            title = r['title']
+            if title.startswith('convention'):
+                year = '1969'
+                title = 'convention'
+            if title.startswith('counterfactuals'):
+                year = '1973'
+                title = 'counterfactuals'
+            elif 'plurality' in title and 'worlds' in title:
+                year = '1986'
+                title = 'on the plurality of worlds'
+            elif 'philos papers' in title:
+                title = title.replace('philos papers', 'philosophical papers')
+            if title != r['title']:
+                print 'updating %s %s' % (r['title'], r['year'])
+                r['year'] = year
+                r['title'] = title
+                changed = True
+    return changed
+
+def update_quine(refs):
+    changed = False
+    for r in refs:
+        if r['author'] == 'quine wvo':
+            title = r['title']
+            if 'two dogmas' in title:
+                year = '1951'
+                title = 'philosophical review - two dogmas of empiricism'
+            if 'methods' in title and 'logic' in title:
+                year = '1950'
+                title = 'methods of logic'
+            if 'logical' in title and 'point' in title and ' v' in title:
+                year = '1953'
+                title = 'from a logical point of view'
+            if 'word' in title and 'object' in title:
+                year = '1960'
+                title = 'word and object'
+            if 'ways' in title and 'paradox' in title:
+                year = '1966'
+                title = 'ways of paradox'
+            elif 'ontological' in title and 'relativ' in title:
+                year = '1969'
+                title = 'ontological relativity and other essays'
+            elif 'roots' in title and 'reference' in title:
+                year = '1974'
+                title = 'roots of reference'
+            elif 'theories' in title and 'things' in title:
+                year = '1981'
+                title = 'theories and things'
+            elif 'philosophy of logic' in title or 'philos logic' in title or 'philosophy logic' in title:
+                year = '1970'
+                title = 'philosophy of logic'
+            if title != r['title']:
+                print 'updating %s %s' % (r['title'], r['year'])
+                r['year'] = year
+                r['title'] = title
+                changed = True
+    return changed
